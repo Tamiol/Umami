fb70a5e4d5dbd6d28e6fafa1431d2712f037892b
Tamiol <gnieznoap@gmail.com>
2023-08-02 17:38:34 +0200

    home page, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './modules/core/core.module';
+import { HomeModule } from './modules/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CoreModule, AppRoutingModule],
+  imports: [BrowserModule, CoreModule, HomeModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
