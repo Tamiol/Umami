acea86436c7801cd027d02312f0a8549f159b3b0
Tamiol <gnieznoap@gmail.com>
2023-08-14 17:37:19 +0200

    created welcome page component

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, WelcomePageComponent],
   imports: [CommonModule, HomeRoutingModule],
   exports: [HomeComponent],
 })
